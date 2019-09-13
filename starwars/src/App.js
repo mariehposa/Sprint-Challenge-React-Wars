b81b3cf54c511abe9cf952aa65e4ab57c3a28f2d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-13 12:28:55 +0100

    set useState and useEffect

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 
 
 const App = () => {
-
-  const 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [starwarState, setStarwarState] = useState();
+
+  useEffect(() =>{
+    axios.get()
+  })
 
   return (
     <div className="App">
