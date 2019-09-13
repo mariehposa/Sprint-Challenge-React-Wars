0af93f221e7595f1684a4ffb6151e3c077757d25
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-13 13:44:54 +0100

    create tags for different atttributes

--- starwars/src/components/StarwarsProfile/starwarsProfile.js
+++ starwars/src/components/StarwarsProfile/starwarsProfile.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Profile ({person}) {
 
+
+function Profile ({person}) {
     return (
         <div>
-            <p>{person.gender}</p>
+            <h3>Name: {person.name}</h3>
+            <p>Gender: {person.gender}</p>
+            <p>Skin Color: {person.skin_color}</p>
+            <p>Height: {person.height}</p>
+            <p>Weight: {person.mass}</p>
         </div>
     )
 }
