0e52d4f6335d97a0b3582fc39d88b4afd54d45d2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-13 14:41:14 +0100

    add styles to characters

---
+++ starwars/src/components/StarwarsProfile/CharacterRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Profile from './starwarsProfile';
+
+const CharacterStyle = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+function CharacterRow ({characters}) {
+    return(
+        <CharacterStyle>
+            {
+                characters.map(character => <Profile person={character}/>)
+            }
+        </CharacterStyle>
+    );
+}
+
+export default CharacterRow;
--- starwars/src/components/StarwarsProfile/starwarsProfile.js
+++ starwars/src/components/StarwarsProfile/starwarsProfile.js
@@ -4,14 +4,42 @@ import styled from 'styled-components';
 
 
 function Profile ({person}) {
+
+    const CharacterStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin: 2.5rem;
+    width: 25%;
+    border: .2rem solid ${person.eye_color.split("-")[0]};
+    border-radius: 2rem;
+    color: white;
+
+    &:hover{
+        border: .2rem solid white;
+        border-radius: 2rem;
+        color: ${person.eye_color.split("-")[0]};
+    }
+
+    h3{
+        color: black;
+        font-family: 'Pacifico', cursive;
+        font-size: 1.5rem;
+
+        &:hover{
+            color:white;
+            font-size: 1.7rem;
+        }
+    }
+`
     return (
-        <div>
-            <h3>Name: {person.name}</h3>
-            <p>Gender: {person.gender}</p>
-            <p>Skin Color: {person.skin_color}</p>
-            <p>Height: {person.height}</p>
-            <p>Weight: {person.mass}</p>
-        </div>
+        <CharacterStyle>
+                <h3>Name: {person.name}</h3>
+                <p>Gender: {person.gender}</p>
+                <p>Skin Color: {person.skin_color}</p>
+                <p>Height: {person.height}</p>
+                <p>Weight: {person.mass}</p>
+        </CharacterStyle>
     )
 }
 
